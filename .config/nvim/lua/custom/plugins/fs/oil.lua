1f59949dea86a9ad8c9d5ed5638be08c6c030cf3
whysosergious <zergski@gmail.com>
2025-03-22 08:50:46 +0100

    weird behaiviour loading custom plugins as module. @todo

--- .config/nvim/lua/custom/plugins/fs/oil.lua
+++ .config/nvim/lua/custom/plugins/fs/oil.lua
@@ -1,10 +1,10 @@
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
-  ---@type oil.SetupOpts
+  -- --@type oil.SetupOpts
   opts = {},
   -- Optional dependencies
-  -- dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -1,3 +1,17 @@
 return {
-  require 'fs/oil',
+  -- require 'fs/oil',
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    -- --@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    config = function()
+      require('oil').setup()
+    end,
+  },
 }
